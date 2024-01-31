9de1aa3cffbfe4eff812244e5ff6bbe69a1ae6ce
Dongwon Shin <edvedv@naver.com>
2024-02-01 00:07:22 +0900

    feat: 레이싱 게임 및 진행 구현

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -4,12 +4,12 @@ import camp.nextstep.edu.missionutils.Console
 
 class InputView {
     companion object {
-        const val MAX_LENGTH = 5
-        const val ERROR_NAME_LENGTH = "자동차의 이름은 5자 이하로 입력해주세요"
-        const val ERROR_NAME_EMPTY = "자동차의 이름이 빈칸입니다"
-        const val ERROR_TIME_EMPTY = "시도 횟수가 빈칸입니다"
-        const val ERROR_TIME_ZERO = "0이 아닌 자연수를 입력해 주세요"
-        const val ERROR_TIME_NOT_NUMBER = "시도 횟수는 숫자만 입력해 주세요"
+        private const val MAX_LENGTH = 5
+        private const val ERROR_NAME_LENGTH = "자동차의 이름은 5자 이하로 입력해주세요"
+        private const val ERROR_NAME_EMPTY = "자동차의 이름이 빈칸입니다"
+        private const val ERROR_TIME_EMPTY = "시도 횟수가 빈칸입니다"
+        private const val ERROR_TIME_ZERO = "0이 아닌 자연수를 입력해 주세요"
+        private const val ERROR_TIME_NOT_NUMBER = "시도 횟수는 숫자만 입력해 주세요"
     }
     fun getCarNames(): List<String> {
         val names = Console.readLine()
--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -4,10 +4,10 @@ import racingcar.Car
 
 class OutputView {
     companion object {
-        const val START = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
-        const val TIME = "시도할 횟수는 몇 회인가요?"
-        const val RESULT = "\n실행 결과"
-        const val WINNER = "최종 우승자 : "
+        private const val START = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+        private const val TIME = "시도할 횟수는 몇 회인가요?"
+        private const val RESULT = "\n실행 결과"
+        private const val WINNER = "최종 우승자 : "
     }
 
     fun printStart() {
@@ -29,10 +29,10 @@ class OutputView {
         println()
     }
 
-    fun printWinners(cars: List<Car>) {
+    fun printWinners(cars: List<String>) {
         var str = "$WINNER"
         for(i in cars.indices) {
-            str += if(i == cars.size) "${cars[i].name}" else "${cars[i].name}, "
+            str += if(i == cars.size - 1) "${cars[i]}" else "${cars[i]}, "
         }
         print(str)
     }
