c3da1f704cd4e43bd2cabd6cd680e2139591800c
Nahyun Kim <hedghog1004@naver.com>
2024-02-02 04:54:28 +0900

    feat: 사용자로부터 자동차 경주에 참여할 자동차 이름을 입력받기

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -1,4 +1,16 @@
 package racingcar.view
 
+import camp.nextstep.edu.missionutils.Console
+import racingcar.config.GameMessageConstant
+import racingcar.service.Converter
+
 class InputView {
+
+    private val converter = Converter()
+
+    fun readRaceCars(): List<String> {
+        println(GameMessageConstant.INPUT_RACE_CARS)
+        val input = Console.readLine()
+        return converter.splitByComma(input)
+    }
 }
