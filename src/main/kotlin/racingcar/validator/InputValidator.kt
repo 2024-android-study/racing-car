f4e4b23954aaf61876f8732d0ab5b6d7a130d157
Nahyun Kim <hedghog1004@naver.com>
2024-02-02 04:37:13 +0900

    feat: 자동차 이름의 글자수를 체크하는 validator 메서드  구현
    글자수가 5 이하일 경우에만 유효

--- src/main/kotlin/racingcar/validator/InputValidator.kt
+++ src/main/kotlin/racingcar/validator/InputValidator.kt
@@ -1,4 +1,11 @@
 package racingcar.validator
 
+import racingcar.config.ErrorMessageConstant
+
 class InputValidator {
+    fun checkCarNameLength(carName: String) {
+        require(carName.length <= 5) {
+            ErrorMessageConstant.ERROR_INVALID_CAR_NAME_LENGTH
+        }
+    }
 }
