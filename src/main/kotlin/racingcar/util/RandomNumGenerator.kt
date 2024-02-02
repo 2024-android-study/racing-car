55c04f3a4717898f8bcb88280264c582763177a2
권민채 <minchae075@naver.com>
2024-02-02 15:12:42 +0900

    fix: 시도횟수 검증 조건 수정 (음수일 경우도 포함)

--- src/main/kotlin/racingcar/util/RandomNumGenerator.kt
+++ src/main/kotlin/racingcar/util/RandomNumGenerator.kt
@@ -3,7 +3,7 @@ package racingcar.util
 import camp.nextstep.edu.missionutils.Randoms
 import racingcar.constants.NumConstant
 
-class RandomNumGenerator {
+object RandomNumGenerator {
     // 랜덤값 생성
     fun generateNum(): Int {
         return Randoms.pickNumberInRange(NumConstant.MIN_RANDOM_NUM, NumConstant.MAX_RANDOM_NUM)
